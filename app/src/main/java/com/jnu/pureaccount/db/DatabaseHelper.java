f6634ee3c8c81c240e975e0e5fd59f1da4a12f55
StreamAzure <1905105036@qq.com>
2021-12-06 17:26:47 +0800

    update:完成数据库中的年、月、日统计表及相关触发器

--- app/src/main/java/com/jnu/pureaccount/db/DatabaseHelper.java
+++ app/src/main/java/com/jnu/pureaccount/db/DatabaseHelper.java
@@ -20,48 +20,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         //SQLite 没有一个单独的用于存储日期和/或时间的存储类，但 SQLite 能够把日期和时间存储为 TEXT值
         String sql1 = "create table item(createTime text, reason int, account double, type int, date text,  year int, month int, day int)";
         //item表：创建日期，类型代号，金额，类型，账目日期，账目年，账目月，账目日
-        String sql2 = "create table daysum(date text, income double, expend double)";
+        String sql2 = "create table daysum(year int, month int, day int, income double, expend double)";
         //单日收支记录表daysum：账目日期，收入，支出
         String sql3 = "create table monthsum(year int, month int, income double, expend double)";
         //月收支记录表monthsum：账目年，账目月，收入，支出
         String sql4 = "create table yearsum(year int, income double, expend double)";
         //年收支记录表yearsum：账目年，收入，支出
 
-        //触发器table_insert，在item插入新项前检查其他表中是否有对应项，没有则创建
-        String sql5="CREATE TRIGGER table_insert BEFORE INSERT ON item\n" +
+        //触发器table_insert，在item 插入 新项前检查其他表中是否有对应项，没有则创建
+        String sql5="CREATE TRIGGER year_insert BEFORE INSERT ON item\n" +
                 "BEGIN\n" +
                 "\tINSERT INTO yearsum (year,expend,income)\n" +
                 "\tSELECT new.year,0,0\n" +
-                "\tWHERE NOT EXISTS(SELECT * FROM yearsum WHERE year=new.year);\n" +
+                "\tWHERE NOT EXISTS(SELECT * FROM yearsum WHERE year=new.year);END\n";
+
+        String sql6 = "CREATE TRIGGER month_insert BEFORE INSERT ON item\n" +
+                 "BEGIN\n" +
                 "\tINSERT INTO monthsum (year,month,expend,income) \n" +
                 "\tSELECT new.year,new.month,0,0\n" +
-                "\tWHERE NOT EXISTS(SELECT * from monthsum where year=new.year and month=new.month);\n" +
+                "\tWHERE NOT EXISTS(SELECT * from monthsum where year=new.year and month=new.month);END\n";
+
+        String sql7 = "CREATE TRIGGER day_insert BEFORE INSERT ON item\n" +
+                "BEGIN\n" +
                 "\tINSERT INTO daysum (year,month,day,expend,income) \n" +
                 "\tSELECT new.year,new.month,new.day,0,0\n" +
                 "\tWHERE NOT EXISTS(SELECT * from daysum where year=new.year and month=new.month and day=new.day);\n" +
                 "END";
 
-        //触发器update_when_edit，在item更新后更新相关表
-        String sql6="CREATE TRIGGER update_when_edit AFTER UPDATE ON item\n" +
-                "BEGIN\n" +
-                "\tUPDATE yearsum set expend = expend + new.account - old.account WHERE new.type = 0 AND year = new.year;\n" +
-                "\tUPDATE yearsum set income = income + new.account - old.account WHERE new.type = 1 AND year = new.year;\n" +
-                "\tUPDATE monthsum set expend = expend + new.account - old.account WHERE new.type = 0 AND year = new.year AND month = new.month;\n" +
-                "\tUPDATE monthsum set income = income + new.account - old.account WHERE new.type = 1 AND year = new.year AND month = new.month;\n" +
-                "\tUPDATE daysum set expend = expend + new.account - old.account WHERE new.type = 0 AND year = new.year AND month = new.month AND day = new.day;\n" +
-                "\tUPDATE daysum set income = income + new.account - old.account WHERE new.type = 1 AND year = new.year AND month = new.month AND day = new.day;\n" +
-                "END";
-
-        //触发器update_when_insert，在item插入新项后更新相关表
-        String sql7="CREATE TRIGGER update_when_insert after insert on item\n" +
-                "BEGIN\n" +
-                "\tUPDATE yearsum set expend = expend + new.account WHERE new.type = 0 AND year = new.year;\n" +
-                "\tUPDATE yearsum set income = income + new.account WHERE new.type = 1 AND year = new.year;\n" +
-                "\tupdate monthsum set expend = expend + new.account where new.type = 0 and year = new.year and month = new.month;\n" +
-                "\tupdate monthsum set income = income + new.account where new.type = 1 and year = new.year and month = new.month;\n" +
-                "\tupdate daysum set expend = expend + new.account where new.type = 0 and year = new.year and month = new.month and day = new.day;\n" +
-                "\tupdate daysum set income = income + new.account where new.type = 1 and year = new.year and month = new.month and day = new.day;\n" +
-                "END";
 
         db.execSQL(sql1);
         db.execSQL(sql2);
@@ -70,13 +55,83 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(sql5);
         db.execSQL(sql6);
         db.execSQL(sql7);
+
+        //触发器，在item 插入 后更新其他表数据
+        createInsertTrigger(db,"year_update_when_insert_e","expend",0,"yearsum");
+        createInsertTrigger(db,"year_update_when_insert_i","income",1,"yearsum");
+        createInsertTrigger(db,"month_update_when_insert_e","expend",0,"monthsum");
+        createInsertTrigger(db,"month_update_when_insert_i","income",1,"monthsum");
+        createInsertTrigger(db,"day_update_when_insert_e","expend",0,"daysum");
+        createInsertTrigger(db,"day_update_when_insert_i","income",1,"daysum");
+
+        //触发器，在item 更新 后更新其他表的数据
+        createUpdateTrigger(db,"year_update_when_edit_e","expend",0,"yearsum");
+        createUpdateTrigger(db,"year_update_when_edit_i","income",1,"yearsum");
+        createUpdateTrigger(db,"month_update_when_edit_e","expend",0,"monthsum");
+        createUpdateTrigger(db,"month_update_when_edit_i","income",1,"monthsum");
+        createUpdateTrigger(db,"day_update_when_edit_e","expend",0,"daysum");
+        createUpdateTrigger(db,"day_update_when_edit_i","income",1,"daysum");
+
+        //触发器，在item 删除 后更新其他表的数据
+        createDeleteTrigger(db,"year_update_when_del_e","expend",0,"yearsum");
+        createDeleteTrigger(db,"year_update_when_del_i","income",1,"yearsum");
+        createDeleteTrigger(db,"month_update_when_del_e","expend",0,"monthsum");
+        createDeleteTrigger(db,"month_update_when_del_i","income",1,"monthsum");
+        createDeleteTrigger(db,"day_update_when_del_e","expend",0,"daysum");
+        createDeleteTrigger(db,"day_update_when_del_i","income",1,"daysum");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 
-    private void createTrigger(String triggerType, int type){
-        //TODO:为什么不支持多语句的触发器，我不理解
+    private void createInsertTrigger(SQLiteDatabase db, String triggerName, String type, int type_code, String tableName){
+        String sql = "CREATE TRIGGER "+triggerName+" after insert on item"+
+                " BEGIN"+
+                " UPDATE "+tableName+" set "+type+" = "+type+" + new.account WHERE new.type = "+type_code;
+        if(tableName.equals("yearsum")){
+            sql = sql+" AND year = new.year;";
+        }
+        else if(tableName.equals("monthsum")){
+            sql = sql+" AND year = new.year AND month = new.month;";
+        }
+        else if(tableName.equals("daysum")){
+            sql = sql+" AND year = new.year AND month = new.month AND day = new.day;";
+        }
+        sql = sql + " END";
+        db.execSQL(sql);
+    }
+
+    private void createDeleteTrigger(SQLiteDatabase db, String triggerName, String type, int type_code, String tableName){
+        String sql = "CREATE TRIGGER "+triggerName+" AFTER DELETE on item"+
+                " BEGIN"+
+                " UPDATE "+tableName+" SET "+type+" = "+type+" - old.account WHERE old.type = "+type_code;
+        if(tableName.equals("yearsum")){
+            sql = sql+" AND year = old.year;";
+        }
+        else if(tableName.equals("monthsum")){
+            sql = sql+" AND year = old.year AND month = old.month;";
+        }
+        else if(tableName.equals("daysum")){
+            sql = sql+" AND year = old.year AND month = old.month AND day = old.day;";
+        }
+        sql = sql + " END";
+        db.execSQL(sql);
+    }
+    private void createUpdateTrigger(SQLiteDatabase db, String triggerName, String type, int type_code, String tableName){
+        String sql = "CREATE TRIGGER "+triggerName+" AFTER UPDATE ON item"+
+                " BEGIN"+
+                " UPDATE "+tableName+" set "+type+" = "+type+" +new.account-old.account WHERE new.type = "+type_code;
+        if(tableName.equals("yearsum")){
+            sql = sql+" AND year = new.year;";
+        }
+        else if(tableName.equals("monthsum")){
+            sql = sql+" AND year = new.year AND month = new.month;";
+        }
+        else if(tableName.equals("daysum")){
+            sql = sql+" AND year = new.year AND month = new.month AND day = new.day;";
+        }
+        sql = sql +" END";
+        db.execSQL(sql);
     }
 }
